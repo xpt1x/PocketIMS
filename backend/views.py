3b6551338f08d1f5726d351b853cbbb2b89bd06f
Jatin <jatinsarda007@gmail.com>
2020-08-31 15:31:38 +0530

    test json response

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from uims_api import SessionUIMS
+from django.http import JsonResponse
+from django.contrib import sessions
 import os
-# Create your views here.
 
+# Create your views here.
 def index(request):
     user = SessionUIMS(os.getenv('UIMS_UID'), os.getenv('UIMS_PASSWORD'))
-    return HttpResponse(user.timetable)
+    return JsonResponse(user.timetable)
