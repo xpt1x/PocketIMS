9a441cbb0decde10f355a0e18b78c8d9d013e396
Jatin <jatinsarda007@gmail.com>
2020-09-27 14:50:06 +0530

    updated requirements

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-import requests, os
+import requests
+import os
 
 url = 'http://127.0.0.1:5000'
 
 if not os.getenv('UIMS_UID'):
     raise RuntimeError('UIMS_UID not set')
 if not os.getenv('UIMS_PASSWORD'):
-    raise RuntimeError('UIMS_UID not set')
+    raise RuntimeError('UIMS_PASSWORD not set')
 
-response = requests.post(f'{url}/api/timetable', {'uid': os.getenv('UIMS_UID'), 'password': os.getenv('UIMS_PASSWORD')})
+response = requests.post(
+    f'{url}/api/timetable', {'uid': os.getenv('UIMS_UID'), 'password': os.getenv('UIMS_PASSWORD')})
 print(response.text)
