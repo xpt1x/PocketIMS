44f66cc5ce9b3c45b4e45248978bfdde89328788
Swarnim Doegar <swarnimdoegar@gmail.com>
2020-09-30 22:33:29 +0530

    Incomplete routing (address and state problem)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import SignIn from './components/SignIn'
 import Dashboard from './components/Dashboard'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect, useHistory} from 'react-router-dom';
 import Message from './components/Message'
 
 function App() {
+  const history = useHistory();
   const [loggedIn, setLoggedIn] = useState(false);
   const [attendance, setAttendance] = React.useState([])
   const [fullAttendance, setFullAttendance] = React.useState([])
@@ -24,8 +25,12 @@ function App() {
     <>
     {message.message ? <Message message={message} /> : null}
     <Switch>
-        <Route path='/' exact render={(props) => <SignIn {...props} {...globalProps}/>}/>
-        <Route path='/dashboard' exact render={(props) => <Dashboard {...props} {...globalProps}/>} />
+        <Route path='/' exact>
+          {loggedIn? <Redirect to='/dashboard'/>:<SignIn {...globalProps}/>}
+        </Route>
+        <Route path='/dashboard' exact>
+          {!loggedIn? <Redirect to={{pathname:"/", state:{from: history.location}}} />:<Dashboard {...globalProps}/>}
+        </Route>
     </Switch>
     </>
   );
