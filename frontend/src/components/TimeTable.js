013bc55197fc0e3459f78fed799eff7f0c627f6c
Jatin <jatinsarda007@gmail.com>
2020-10-15 21:06:28 +0530

    dynamic population of timetable entries

--- frontend/src/components/TimeTable.js
+++ frontend/src/components/TimeTable.js
@@ -13,7 +13,6 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import Loading from "./common/Loading";
-import { flexbox, style } from "@material-ui/system";
 
 const useColorlibStepIconStyles = makeStyles({
   root: {
@@ -33,28 +32,11 @@ const useColorlibStepIconStyles = makeStyles({
     transform: "scale(1.5)",
     backgroundColor: "#ff5722",
     boxShadow: "0px 0px 20px #ff5744aa",
-    transition: ".2s"
+    transition: ".2s",
   },
 });
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    // width: "300px",
-  },
-
-  content: {
-    // marginTop: theme.spacing(-1),
-  },
-
-  div: {
-    // top: "80px",
-    // width: "300px",
-    // marginLeft: "15%",
-    // position: "absolute",
-    // display: "flex",
-    // justifyContent: "left",
-  },
-
   time_table: {
     position: "relative",
     top: "80px",
@@ -84,22 +66,29 @@ const useStyles = makeStyles((theme) => ({
 
   card: {
     marginBottom: "20px",
-    boxShadow: "0px 6px 6px -3px rgba(0,0,0,0.2), 0px 10px 14px 1px rgba(0,0,0,0.14), 0px 4px 18px 3px rgba(0,0,0,0.12)",
-  }
-
+    boxShadow:
+      "0px 6px 6px -3px rgba(0,0,0,0.2), 0px 10px 14px 1px rgba(0,0,0,0.14), 0px 4px 18px 3px rgba(0,0,0,0.12)",
+  },
+  connector: {
+    width: "2px",
+    height: "60px",
+    backgroundColor: "#ededed",
+  },
 }));
+
+const Days = {
+  1: "Mon",
+  2: "Tue",
+  3: "Wed",
+  4: "Thu",
+  5: "Fri",
+  6: "Sat",
+  7: "Sun",
+};
 
 function Icon(props) {
   const classes = useColorlibStepIconStyles();
-  const Days = {
-    1: "Mon",
-    2: "Tue",
-    3: "Wed",
-    4: "Thu",
-    5: "Fri",
-    6: "Sat",
-    7: "Sun",
-  };
+
   return (
     <div
       className={clsx(classes.root, {
@@ -111,19 +100,9 @@ function Icon(props) {
   );
 }
 
-
 function Connector() {
-  return (
-    <div
-      style={{
-        width: "2px",
-        height: "60px",
-        backgroundColor: "#ededed",
-        // position: "relative",
-        // left: "-4px",
-      }}
-    ></div>
-  );
+  const classes = useStyles();
+  return <div className={classes.connector} />;
 }
 
 function getPresentDay() {
@@ -138,8 +117,6 @@ function TimeTable(props) {
   const handleStep = (step) => () => {
     setActiveStep(step - 1);
   };
-  const arr = [1, 2, 3, 4, 5, 6, 7];
-  const day_map = { Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6, Sun: 7 };
 
   return props.timetable ? (
     <Container className={styles.time_table}>
@@ -150,73 +127,43 @@ function TimeTable(props) {
         activeStep={activeStep}
         className={styles.stepper}
       >
-        {arr.map((elm) => (
+        {Object.keys(Days).map((elm) => (
           <Step key={elm} className={styles.step}>
             <StepLabel StepIconComponent={Icon} onClick={handleStep(elm)} />
           </Step>
         ))}
       </Stepper>
       <div className={styles.card_wrapper}>
-        <Card className={styles.card}>
-          <CardHeader
-            title="Subject Name"
-            subheader="Teacher name"
-            avatar={<Avatar>A</Avatar>}
-          />
-          <CardContent className="">
-            <Typography variant="body2" color="textSecondary">
-              Timings: XYZ - [L]
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className={styles.card}>
-          <CardHeader
-            title="Subject Name"
-            subheader="Teacher name"
-            avatar={<Avatar>A</Avatar>}
-          />
-          <CardContent className="">
-            <Typography variant="body2" color="textSecondary">
-              Timings: XYZ - [L]
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className={styles.card}>
-          <CardHeader
-            title="Subject Name"
-            subheader="Teacher name"
-            avatar={<Avatar>A</Avatar>}
-          />
-          <CardContent className="">
-            <Typography variant="body2" color="textSecondary">
-              Timings: XYZ - [L]
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className={styles.card}>
-          <CardHeader
-            title="Subject Name"
-            subheader="Teacher name"
-            avatar={<Avatar>A</Avatar>}
-          />
-          <CardContent className="">
-            <Typography variant="body2" color="textSecondary">
-              Timings: XYZ - [L]
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className={styles.card}>
-          <CardHeader
-            title="Subject Name"
-            subheader="Teacher name"
-            avatar={<Avatar>A</Avatar>}
-          />
-          <CardContent className="">
-            <Typography variant="body2" color="textSecondary">
-              Timings: XYZ - [L]
-            </Typography>
-          </CardContent>
-        </Card>
+        {Object.keys(
+          props.timetable[Days[activeStep + 1]] !== undefined
+            ? props.timetable[Days[activeStep + 1]]
+            : [""]
+        ).map((key, id) => {
+          if (props.timetable[Days[activeStep + 1]] === undefined)
+            return (
+              <Card id={id} className={styles.card}>
+                <CardHeader title="Wohooooo!" subheader="Enjoy today" />
+                <CardContent>No classes on this day</CardContent>
+              </Card>
+            );
+          const sub = props.timetable[Days[activeStep + 1]][key];
+          return sub !== null ? (
+            <Card key={id} className={styles.card}>
+              <CardHeader
+                title={sub.title}
+                subheader={sub.teacher}
+                avatar={<Avatar>{sub.title[0]}</Avatar>}
+              />
+              <CardContent>
+                <Typography variant="body2" color="textPrimary">
+                  Timings: {key} - [{sub.type}]
+                </Typography>
+              </CardContent>
+            </Card>
+          ) : (
+            <hr key={id} />
+          );
+        })}
       </div>
     </Container>
   ) : (
