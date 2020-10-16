6dbd1f284f98ca7dbaabe3a335b28012641a25c6
Jatin <jatinsarda007@gmail.com>
2020-10-16 11:09:23 +0530

    changes for test deploy

--- frontend/src/ApiLayer/Api.js
+++ frontend/src/ApiLayer/Api.js
@@ -1,4 +1,13 @@
-import {create} from 'apisauce'
+import { create } from "apisauce";
 
-const Api = create({baseURL: 'http://127.0.0.1:5000/api'})
-export default Api
+//const Api = create({baseURL: 'http://127.0.0.1:5000/api'})
+const localURL = "http://127.0.0.1:5000/api";
+const productionURL = "";
+const Api = create({
+  baseURL:
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? localURL
+      : productionURL,
+});
+
+export default Api;
