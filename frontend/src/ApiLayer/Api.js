948a29d6d7d9c307a15bf1298738bf110d4b0714
Jatin <jatinsarda007@gmail.com>
2020-10-16 14:31:43 +0530

    fixed prod url

--- frontend/src/ApiLayer/Api.js
+++ frontend/src/ApiLayer/Api.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const localURL = "http://127.0.0.1:5000/api";
-const productionURL = "https://web-compiler.herokuapp.com/api";
+const productionURL = "https://pocketims.herokuapp.com/api";
 const Api = create({
   baseURL:
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
