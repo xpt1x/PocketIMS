6be1cfb05283a6141f7dcabb704cf655f8fdf7da
Jatin <jatinsarda007@gmail.com>
2020-10-18 21:27:45 +0530

    updated version

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -12,7 +12,7 @@
 
 //Change the below comment to refresh the service worker on update
 //App Version: 2.0
-const version = "2-b";
+const version = "2-c";
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
