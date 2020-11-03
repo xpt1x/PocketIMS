82f1fe67122a91cb0aa2cca5c38baa6147e8e453
xpt1x <jatinsarda007@gmail.com>
2020-11-03 16:47:12 +0530

    changed host

--- frontend/src/ApiLayer/Api.js
+++ frontend/src/ApiLayer/Api.js
@@ -1,7 +1,9 @@
 import { create } from "apisauce";
 
 const localURL = "http://127.0.0.1:5000/api";
-const productionURL = "https://pocketims.herokuapp.com/api";
+//const productionURL = "https://pocketims.herokuapp.com/api";
+const productionURL = "http://143.110.240.76:5000/api";
+
 const Api = create({
   baseURL:
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
